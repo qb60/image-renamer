1b59ed00c02b2d36190025d53e1e1a23ba035b9f
Sergey Cheremisov <q.basic60@gmail.com>
2024-09-27 16:56:06 +0500

    feat: make output pattern more clear

--- img-renamer.py
+++ img-renamer.py
@@ -5,30 +5,28 @@ import os
 import re
 from datetime import datetime
 
-# Define the output format as a constant
-OUTPUT_FORMAT = "IMG_{YYYY}{MM}{DD}_{hh}{mm}{ss}{suffix}"
+OUTPUT_FORMAT = "IMG_{YYYY}{MM}{DD}_{hh}{mm}{ss}"
 
-# Updated patterns without trailing dots and asterisks
-patterns = [
-    r'IMG{YYYY}{MM}{DD}{hh}{mm}{ss}',
-    r'IMG_{DD}-{MM}-{YYYY}_{hh}-{mm}-{ss}',
-    r'IMG_{YYYY}{MM}{DD}_{hh}{mm}{ss}',
-    r'PXL_{DD}-{MM}-{YYYY}_{hh}-{mm}-{ss}',
-    r'PXL_{YYYY}{MM}{DD}_{hh}{mm}{ss}',
-    r'SK_{YYYY}{MM}{DD}_{hh}{mm}{ss}'
+INPUT_FORMATS = [
+    "IMG{YYYY}{MM}{DD}{hh}{mm}{ss}",
+    "IMG_{DD}-{MM}-{YYYY}_{hh}-{mm}-{ss}",
+    "IMG_{YYYY}{MM}{DD}_{hh}{mm}{ss}",
+    "PXL_{DD}-{MM}-{YYYY}_{hh}-{mm}-{ss}",
+    "PXL_{YYYY}{MM}{DD}_{hh}{mm}{ss}",
+    "SK_{YYYY}{MM}{DD}_{hh}{mm}{ss}"
 ]
 
 def create_regex_pattern(pattern):
-    return pattern.replace(
+    return (pattern.replace(
         '{YYYY}', r'(?P<YYYY>\d{4})').replace(
         '{MM}', r'(?P<MM>\d{2})').replace(
         '{DD}', r'(?P<DD>\d{2})').replace(
         '{hh}', r'(?P<hh>\d{2})').replace(
         '{mm}', r'(?P<mm>\d{2})').replace(
-        '{ss}', r'(?P<ss>\d{2})') + r'.*'  # Add '.*' at the end to match any trailing characters
+        '{ss}', r'(?P<ss>\d{2})') + r'.*')  # Add '.*' at the end to match any trailing characters
 
 # Convert human-readable patterns to regex patterns
-regex_patterns = [create_regex_pattern(pattern) for pattern in patterns]
+regex_patterns = [create_regex_pattern(pattern) for pattern in INPUT_FORMATS]
 
 def rename_file(filename, existing_files):
     name, ext = os.path.splitext(filename)
@@ -39,7 +37,7 @@ def rename_file(filename, existing_files):
             suffix = ""
             counter = 1
             while True:
-                new_name = OUTPUT_FORMAT.format(**date_parts, suffix=suffix) + ext
+                new_name = OUTPUT_FORMAT.format(**date_parts) + suffix + ext
                 if new_name not in existing_files:
                     return new_name
                 suffix = f"_{counter}"
